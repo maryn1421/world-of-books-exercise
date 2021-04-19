205eb6b17c40e40f566f35cf7313a76a2d19f49d
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-04-19 15:25:59 +0200

    added fields to listing entity

--- src/main/java/com/worldofbooks/exercise/model/Listing.java
+++ src/main/java/com/worldofbooks/exercise/model/Listing.java
@@ -5,6 +5,8 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.Currency;
+import java.util.Date;
 import java.util.UUID;
 
 @Entity
@@ -15,7 +17,24 @@ public class Listing {
     @Id
     private UUID id;
 
+    private String title;
 
+    private String description;
 
+    private UUID inventory_item_location_id;
+
+    private Currency listing_price;
+
+    private String currency;
+
+    private Integer quantity;
+
+    private Long listing_status;
+
+    private Long marketplace;
+
+    private Date upload_time;
+
+    private String owner_email_address;
 
 }
