31f9c243c434d7c18f9593e4c02e6985b9bbfe2c
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-04-22 10:57:39 +0200

    added 2 new utility classes: Filehandler and ErrorHandler

---
+++ src/main/java/com/worldofbooks/exercise/utility/ErrorHandler.java
@@ -0,0 +1,7 @@
+package com.worldofbooks.exercise.utility;
+
+public class ErrorHandler {
+
+
+
+}
---
+++ src/main/java/com/worldofbooks/exercise/utility/FileHandler.java
@@ -0,0 +1,15 @@
+package com.worldofbooks.exercise.utility;
+
+import java.io.File;
+
+public class FileHandler {
+
+
+
+    public void writeToFile(File file){
+        
+    }
+
+
+
+}
