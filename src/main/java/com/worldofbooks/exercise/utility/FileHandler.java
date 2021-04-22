9817e5df31a1e133ec0593cbe5e04d9672c0c439
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-04-22 11:04:10 +0200

    created import errors model and its fields

--- src/main/java/com/worldofbooks/exercise/utility/FileHandler.java
+++ src/main/java/com/worldofbooks/exercise/utility/FileHandler.java
@@ -1,13 +1,39 @@
 package com.worldofbooks.exercise.utility;
 
+import com.opencsv.CSVWriter;
+import com.worldofbooks.exercise.model.ImportError;
+
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FileHandler {
 
 
 
-    public void writeToFile(File file){
-        
+    public void writeToFile(File file, ImportError error){
+        try {
+            FileWriter outputfile = new FileWriter(file);
+
+            CSVWriter writer = new CSVWriter(outputfile);
+
+            String[] header = { "Name", "Class", "Marks" };
+            writer.writeNext(header);
+
+            // add data to csv
+            /*
+            String[] data1 = { "Aman", "10", "620" };
+            writer.writeNext(data1);
+            String[] data2 = { "Suraj", "10", "630" };
+            writer.writeNext(data2);
+
+             */
+            // closing writer connection
+            writer.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
